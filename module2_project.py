d55aaabc53cb464af4307dd8036b0fe66f6e6537
art-school-wannabe <84336475+art-school-wannabe@users.noreply.github.com>
2021-09-13 22:16:38 -0400

    Add files via upload

---
+++ module2_project.py
@@ -0,0 +1,32 @@
+
+# jagger adams
+# 09.13.21
+# module 2 project
+
+# set variables 
+
+# set maximum and minimum weight
+max_weight = 100
+min_weight = 0.25
+
+# set price per pound
+ppp = 7.99
+
+# customer order 
+weight = round(float(input("Enter cheese order weight (numeric value): ")),2)
+
+
+# item avalibility conditional statement
+
+# oversized order
+if weight > 100:
+    print(f"{weight} lbs is more than currently is available in stock.")
+
+# undersized order
+elif weight < 0.25:
+    print(f"{weight} lbs is below the minimum order amount.")
+    
+# fufilled order
+else:
+    total = round((weight*ppp),2)
+    print(f"{weight} lbs of cheese costs ${total}.")
