1bf223e174d698cd5d53ea15798372c071b0e38b
art-school-wannabe <84336475+art-school-wannabe@users.noreply.github.com>
2022-02-01 10:49:51 -0500

    Update module5_project.py

--- module5_project.py
+++ module5_project.py
@@ -1,8 +1,32 @@
 
+# define a function that searches a given file for a given keyword
+# and return the line and index of keyword in the text
 def file_search(file_name, keyword):
+    # open file and set it to a variable
     file = (open(file_name)).read()
+    # the keyword is in the text
     if keyword in file:
-        index = file.index(keyword)
-        return (f"The word is at index {index}.")
+        # split the text into a list of lines
+        lines = file.splitlines()
+        # find the line the keyword is on
+        for l in lines:
+            if keyword in l:
+                line_index = lines.index(l)
+                # split the line into words
+                words = lines[line_index].split()
+                # find the index of the word in the line
+                for w in words:
+                    if keyword in w:
+                        word_index = words.index(w)
+                        break
+                    else:
+                        continue
+                break
+            else:
+                continue
+        # return the line and index
+        return (f"The word is on line {line_index + 1} at index {word_index + 1}")
+    # the keyword is not in the file
     else:
         return ("The file does not contain that word.")
+    
